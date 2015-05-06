283dedc40c245fc2ab8a6a43ce2281e4f5f43f94
Aiy <Aiy@PC>
2015-05-06 21:17:31 +0700

    added action for starting Anhar063

---
+++ src/Inheritance/ReadMe.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Inheritance;
+
+/**
+ *
+ * @author Aiy
+ */
+public class ReadMe {
+    
+   // Project Minggu ke2 Untuk penilaian Inheritance atau Pewarisan
+    
+}
